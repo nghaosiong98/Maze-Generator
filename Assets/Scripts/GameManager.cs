cd8ac98e2aace86ee0300c01f4834d1d442bf832
kfkok <kfkok4@yahoo.com>
2019-10-07 16:12:31 +0800

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+    private Maze mazeInstance;
+
+    private void Awake()
+    {
+        StartCoroutine(BeginGame());
+    }
+
+    private IEnumerator BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab, new Vector3(0.5f, 0f, 0.5f), Quaternion.identity) as Maze;
+        yield return mazeInstance.Generate();
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        StartCoroutine(BeginGame());
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+}
